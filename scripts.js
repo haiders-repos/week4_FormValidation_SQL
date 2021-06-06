1dbcc6053d037675654323cef699dafa93962d0a
haider <haiders-repos@protonmail.com>
2021-06-06 16:13:48 +0100

    updated form validation behaviour

--- scripts.js
+++ scripts.js
@@ -1,19 +1,58 @@
-const name=document.getElementById('name');
+const firstname=document.getElementById('name');
+const surname=document.getElementById('surname');
+const mail=document.getElementById('usermail');
+const message=document.getElementById('usermessage');
 const form=document.getElementById('contact-form');
-const errorElement=document.getElementById('error');
+const nameErrorElement=document.getElementById('nameError');
+const surnameErrorElement=document.getElementById('surnameError');
+const mailErrorElement=document.getElementById('mailError');
+const messageErrorElement=document.getElementById('messageError');
+
 
 form.addEventListener('submit',(e)=>{
-    let messages=[];
+    // let messages=[];
 
-    if(name.value==='' || name.value==null ){
-        messages.push('Name Is Required');
+    if(firstname.value ==='' || firstname.value==null ){
+        e.preventDefault();
+        nameErrorElement.innerText="First Name Is Required"
     }
 
-    if(messages.length>0){
-    e.preventDefault();
-    errorElement.innerText=messages.join(",");
+    if(firstname.value  != '' ){
+       
+        nameErrorElement.innerText='';
     }
 
+    if(surname.value==='' || surname.value==null ){
+        e.preventDefault();
+        surnameErrorElement.innerText="Surname Is Required"
+    }
+
+    if(surname.value  != '' ){
+        
+        surnameErrorElement.innerText='';
+    }
+
+    if(mail.value==='' || mail.value==null ){
+        e.preventDefault();
+        mailErrorElement.innerText="Email Is Required"
+    }
+
+    if(mail.value  != '' ){
+       
+        mailErrorElement.innerText='';
+    }
+
+  
+    if(message.value==='' || message.value==null ){
+        e.preventDefault();
+        messageErrorElement.innerText="Please provide information about your query."
+        
+    }
+
+    if(message.value  != '' ){
+        
+        messageErrorElement.innerText='';
+    }
    
 })
     
