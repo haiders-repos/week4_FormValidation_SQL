bcc70d71ea986d42242d29b3e18c2dc6b53af764
haider <haiders-repos@protonmail.com>
2021-06-06 15:04:41 +0100

    implemented basic form validation functionality

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
 const name=document.getElementById('name');
-const password=document.getElementById('password');
 const form=document.getElementById('contact-form');
+const errorElement=document.getElementById('error');
+
+form.addEventListener('submit',(e)=>{
+    let messages=[];
+
+    if(name.value==='' || name.value==null ){
+        messages.push('Name Is Required');
+    }
+
+    if(messages.length>0){
+    e.preventDefault();
+    errorElement.innerText=messages.join(",");
+    }
+
+   
+})
+    
